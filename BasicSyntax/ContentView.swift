5f4c2251d6e66e2c057f5405b9a58f354465a71b
Alex McPherson <alex.mcpherson@worldpay.com>
2020-06-12 14:02:00 +0100

    minMidMax geometry using geometry reader

--- BasicSyntax/ContentView.swift
+++ BasicSyntax/ContentView.swift
@@ -10,42 +10,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10) {
-            Text("Geometry Reader").font(.largeTitle)
-            Text("Getting Coordinates").foregroundColor(.gray)
-            Text("Getting the coordinates (x, y) of a geometry view is little different. Take a look at this example:")
+        VStack(spacing: 20) {
+            Text("GeomtryReader").font(.largeTitle)
+            Text("Min Mid Max").foregroundColor(.gray)
+            Text("You can also get the minimum (min), middle (mid), and maximum (max) X and Y coordinate from the geometry reader.")
+                .layoutPriority(1)
 
             GeometryReader { geometry in
-                VStack(spacing:10) {
-                    Text("X: \(geometry.frame(in: .local).origin.x)")
-                    Text("Y: \(geometry.frame(in: .local).origin.y)")
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Local Coordinate Space")
+                    HStack(spacing: 10) {
+                        Text("MinX: \(Int(geometry.frame(in: .local).minX))")
+                        Spacer()
+                        Text("MidX: \(Int(geometry.frame(in: .local).midX))")
+                        Spacer()
+                        Text("MaxX: \(Int(geometry.frame(in: .local).maxX))")
+                    }
+                    Text("Global Coordinate Space")
+                    HStack(spacing: 10) {
+                        Text("MinX: \(Int(geometry.frame(in: .global).minX))")
+                        Spacer()
+                        Text("MidX: \(Int(geometry.frame(in: .global).midX))")
+                        Spacer()
+                        Text("MaxX: \(Int(geometry.frame(in: .global).maxX))")
+                    }
                 }
-                .foregroundColor(.white)
+                .padding(.horizontal)
             }
-            .background(Color.pink)
-
-            Text("The local coordinate space will always give you zeros.")
-            Text("You need to look globally to get the coordinates inside the current view:")
-
-            GeometryReader { geomtry in
-                VStack(spacing:10) {
-                    Text("X: \(geomtry.frame(in: .global).origin.x)")
-                    Text("Y: \(geomtry.frame(in: .global).origin.y)")
-                }
-                .foregroundColor(.white)
-            }
-            .background(Color.pink)
             .frame(height: 200)
+            .foregroundColor(.white)
+            .background(Color.pink)
 
+            HStack {
+                GeometryReader { geometry in
+                    VStack(spacing: 10) {
+                        Text("MinX: \(Int(geometry.frame(in: .global).minX))")
+                        Spacer()
+                        Text("MidX: \(Int(geometry.frame(in: .global).midX))")
+                        Spacer()
+                        Text("MaxX: \(Int(geometry.frame(in: .global).maxX))")
+                    }
+                    .padding(.vertical)
+                }
+                .foregroundColor(.white)
+                .background(Color.pink)
+
+                Image("kb")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
         }
         .font(.title)
-        .padding(.horizontal)
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-//            .previewDevice("iPad Pro (9.7-inch)")
+            .previewDevice("iPad Pro (11-inch)")
     }
 }
