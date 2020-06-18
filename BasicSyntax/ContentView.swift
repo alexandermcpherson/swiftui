68307c298df6417425aebb74926f7cab59b00185
Alex McPherson <alex.mcpherson@worldpay.com>
2020-06-18 13:48:23 +0100

    Text Composition

--- BasicSyntax/ContentView.swift
+++ BasicSyntax/ContentView.swift
@@ -13,42 +13,33 @@ struct ContentView: View {
         VStack(spacing: 20) {
             Text("Button")
                 .font(.largeTitle)
-            Text("Introduction")
-                .font(.title)
+            Text("Text Composition")
                 .foregroundColor(.gray)
-            Text("If you just want to show the default text style in a button then you can pass a string as the first parameter")
+            Text("You can add more than one text view to a button. By default, the views are composed within an HStack")
+                .frame(maxWidth:.infinity)
+                .padding()
+                .background(Color.purple)
+                .foregroundColor(.white)
+                .font(.title)
+
+            Button(action: {}, label: {
+                Text("Forgot Password")
+                Text("Tap to Recover").foregroundColor(.orange)
+            })
+
+            Text("User a VStack")
                 .frame(maxWidth: .infinity)
                 .padding()
                 .background(Color.purple)
                 .foregroundColor(.white)
-            
-            Button("Default Button Style") {}
 
-            Text("You can customize the text shown for a button")
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.purple)
-                .foregroundColor(.white)
-
-            Button(action: {
-                // Button action code here
-                print("Alex")
-            }) {
-                Text("Headline Font")
-                    .font(.headline)
-            }
-
-            Divider()
-
-            Button(action: {}) {
-                Text("Foreground Color").foregroundColor(.red)
-            }
-
-            Divider()
-
-            Button(action: {}) {
-                Text("Thin font weight").fontWeight(.thin)
-            }
+            Button(action: {}, label: {
+                VStack {
+                    Text("New User")
+                    Text("Register Here")
+                        .font(.body)
+                }
+            })
         }
     }
 }
