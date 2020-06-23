09a3b76592d54b5f5f4c8b2afff3ec586b280efc
Alex McPherson <alex.mcpherson@worldpay.com>
2020-06-23 13:59:32 +0100

    With minimum and maximum date range

--- BasicSyntax/ContentView.swift
+++ BasicSyntax/ContentView.swift
@@ -9,33 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var nextFullMoodDate = Date()
 
-    @State private var arrivalDate = Date()
-    let mainColor = Color("LightPinkColor")
+    var withinNext30Days: ClosedRange<Date> {
+        let today = Calendar.current.date(byAdding: .minute, value: -1, to: Date())!
+
+        let next30Days = Calendar.current.date(byAdding: .day, value: 30, to: Date())!
+
+        return today...next30Days
+    }
 
     var body: some View {
-        VStack(spacing: 20) {
-            Text("DatePicker")
-                .font(.largeTitle)
-            Text("To Date or Time")
-                .foregroundColor(.gray)
-            Text("You can set a maximum date or time that you cannot go past with the date picker")
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(mainColor)
-                .foregroundColor(.white)
+        VStack(spacing: 30) {
+            Text("DatePicker").font(.largeTitle)
+            Text("Min and Max Date Range").foregroundColor(.gray)
+            HStack {
+                Spacer()
+                Image(systemName: "moon.circle")
+                Spacer()
+                Circle().frame(width: 60, height: 60)
+                Spacer()
+                Image(systemName: "moon.circle.fill")
+                Spacer()
+            }
+            .foregroundColor(.yellow)
 
-            Image("kb")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+            Text("Select date of next full moon").font(.title)
+            DatePicker("", selection: $nextFullMoodDate, in: withinNext30Days, displayedComponents: .date)
+                .padding(.horizontal, 28)
+                .background(RoundedRectangle(cornerRadius: 10).foregroundColor(.yellow))
+                .shadow(radius: 20, y:20).labelsHidden()
 
-            Text("Date and Time of Birth")
-
-            DatePicker("", selection: $arrivalDate, in: ...Date(),
-                       displayedComponents: [.date, .hourAndMinute])
-                .background(RoundedRectangle(cornerRadius: 20).stroke(mainColor, lineWidth:2))
-                .padding(.horizontal)
-                .labelsHidden()
+            Text("(Valid range only in the next 30 days)")
+                .padding(.vertical)
+            Spacer()
         }
         .font(.title)
     }
