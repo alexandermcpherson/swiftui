f114c9046c09fe0ccf82efd6d49c5fd2688aeb8a
Alex McPherson <alex.mcpherson@worldpay.com>
2020-07-14 18:45:50 +0100

    Simple combine stuff

--- BasicSyntax/ContentView.swift
+++ BasicSyntax/ContentView.swift
@@ -8,38 +8,25 @@
 
 import SwiftUI
 
+class ResetPasswordModel: ObservableObject {
+  @Published var email = ""
+}
+
 struct ContentView: View {
+
+    @ObservedObject private var model = ResetPasswordModel()
+
     var body: some View {
-        VStack(spacing: 10) {
-            Text("Geometry Reader").font(.largeTitle)
-            Text("Getting Coordinates").foregroundColor(.gray)
-            Text("Getting the coordinates (x, y) of a geometry view is little different. Take a look at this example:")
-
-            GeometryReader { geometry in
-                VStack(spacing:10) {
-                    Text("X: \(geometry.frame(in: .local).origin.x)")
-                    Text("Y: \(geometry.frame(in: .local).origin.y)")
+        Form {
+            Section {
+                TextField("Email", text: $model.email)
+                Button(action: {
+                    print(self.model.email)
+                }) {
+                    Text("Press me")
                 }
-                .foregroundColor(.white)
             }
-            .background(Color.pink)
-
-            Text("The local coordinate space will always give you zeros.")
-            Text("You need to look globally to get the coordinates inside the current view:")
-
-            GeometryReader { geomtry in
-                VStack(spacing:10) {
-                    Text("X: \(geomtry.frame(in: .global).origin.x)")
-                    Text("Y: \(geomtry.frame(in: .global).origin.y)")
-                }
-                .foregroundColor(.white)
-            }
-            .background(Color.pink)
-            .frame(height: 200)
-
         }
-        .font(.title)
-        .padding(.horizontal)
     }
 }
 
